54600d9c54fe54690aefe1d49f4f546c58e8d95f
zhangbocheng <zhangbocheng@joyme.sg>
2023-08-22 16:01:56 +0800

    MyBlog-新增工程

---
+++ MyBlog/MyBlog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyBlog
+//
+//  Created by bocheng.zhang on 2023/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
